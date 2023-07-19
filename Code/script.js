6f024850dc4aea523e9039ac61ce1818412521f2
ahmell <andrewmell10@gmail.com>
2023-07-19 16:41:09 -0500

    added color functionality to time blocks

--- Code/script.js
+++ Code/script.js
@@ -1,5 +1,3 @@
-console.log('hello world')
-
 var timeGrid = document.querySelector('#timeGrid');
 
 const hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
@@ -38,7 +36,17 @@ function numberTranslate(hour) {
     }
 }
 
+function timeSet(hour){
+  const currentTime = new Date().getHours()
 
+    if (hour > currentTime){
+        return "future"
+    } else if (hour == currentTime){
+        return "present"
+    } else {
+        return "past"
+    }
+}
 
 for (let index = 0; index < hours.length; index++) {
     var outerDiv = document.createElement('div')
@@ -46,7 +54,7 @@ for (let index = 0; index < hours.length; index++) {
 
     outerDiv.innerHTML = `
   <div class="col-2 col-md-1 hour text-center py-3 ">${numberTranslate(hours[index])}</div>
-        <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+        <textarea class="col-8 col-md-10 description ${timeSet(hours[index])}" rows="3"> </textarea>
         <button class="btn saveBtn col-2 col-md-1" aria-label="save">
           <i class="fas fa-save" aria-hidden="true"></i>
         </button>
