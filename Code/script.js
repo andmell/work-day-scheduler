b5e6aa092bb79e6040ea1008dce7fe0178b5312e
ahmell <andrewmell10@gmail.com>
2023-07-19 21:10:05 -0500

    intermediate touch-up of code, removing redundant comments, polishing...

--- Code/script.js
+++ Code/script.js
@@ -1,14 +1,18 @@
+// This first variable will be used to refer to the parent element that is housing all of the time slots.
 var timeGrid = document.querySelector('#timeGrid');
+// This variable is used to set and update the time found in the header. 
 var clockTime = document.querySelector('#currentDay');
 
-
+// This array will help us define the time listed on the left column of the slots. It will also make it easier to check against the user's local time, as they are ascending numbers in 24hr time.
 const hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
+// This function will set the time based on the user's local computer time, and is recalled every second to show a consistently updating time.
+// The new Date variable will reference the date and time based on the user's time, and the .toLocaleString will further refine the variable to only show the date and time, not any information based on time zone, etc.
 setInterval(() => {
   clockTime.innerHTML = new Date().toLocaleString()
 }, 1000)
 
-
+// This function will switch out each number in the hours array, and return a string that resembles a more user-friendly format of the time.
 function numberTranslate(hour) {
   switch (hour) {
     case 9:
@@ -43,7 +47,9 @@ function numberTranslate(hour) {
   }
 }
 
+// This function, when run, will compare the current hour, with the hour assigned to each individual text box, and will return past, present, or future depending on the result. 
 function timeSet(hour) {
+  // We're setting a constant to equal the current hour.
   const currentTime = new Date().getHours()
 
   if (hour > currentTime) {
@@ -55,10 +61,15 @@ function timeSet(hour) {
   }
 }
 
+// This for-loop creates time slots depending on how long the hours array is.
 for (let index = 0; index < hours.length; index++) {
   var outerDiv = document.createElement('div')
   outerDiv.classList.add('row', 'time-block')
 
+// The inner HTML for the newly created time-slots is the same as was found in the original code. The template literals allows us to create dynamic time boxes that change based on local information.
+// For the left hand column that shows the hour, we run our numberTranslate function through our hours array, and allows us to change the data based on where in the index it's found.
+// The actual text area, we run our timeSet function through the hours array, which changes the color of the grid.
+// The text input from the user is pulled by using the final template literal, which pulls a string from the local storage. If no string is detected, it will return with an empty string, as defined by the || ''.
   outerDiv.innerHTML = `
   <div class="col-2 col-md-1 hour text-center py-3 ">${numberTranslate(hours[index])}</div>
         <textarea class="col-8 col-md-10 description ${timeSet(hours[index])}" rows="3">${localStorage.getItem(hours[index]) || ''}</textarea>
@@ -66,13 +77,18 @@ for (let index = 0; index < hours.length; index++) {
           <i class="fas fa-save" aria-hidden="true"></i>
         </button>
   `
-
+// As we finish coding what will happen to the time slots, we append them to the parent element.
   timeGrid.append(outerDiv);
 }
 
+// These next variables will be used to add functionality to the save buttons and text area's of our scheduler. 
 const buttons = document.querySelectorAll('.saveBtn')
 const textArea = document.querySelectorAll('.description');
 
+
+// We use a for-loop to assign an event listener to each button based on their index value, same as the time slots. 
+// The console.log methods are there for my own personal debugging.
+// One the save button is clicked, any text saved in the respective time slot, will be saved to local storage. It will be pulled and displayed again in the above function.
 for (let index = 0; index < buttons.length; index++){
     buttons[index].addEventListener("click", ()=>{
         console.log(hours[index])
--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # work-day-scheduler
-This app is meant to help schedule your workday from 9am through 5pm.
 
+## Description
+This app was built to assist a user in organizing their 9 to 5 workday. It allows a user to input text to time blocks, which will change color depending on if the time block is set in the future, past, or is currently active, depending on the user's local time. 
+
+This application is JavaScript heavy, and relies heavily on third party libraries such as Jquery and Bootstrap. It was an interesting challenge, being able to navigate those libraries to enhance the function of the app. 
+
+## Installation
+This is an application that has been deployed on GitHub's native webpage deployment software, and can be navigated to [here](). No installation is necessary.
+
+## Usage
+Once navigated to the application, you may see differing colored blocks depending on your local time. A green block indicates a time that is at least 1 hour in the future. A red block indicates an hour that is currently being passed. A gray block is a time that has already passed.
+
+For example, if a user's time is 11:01 AM, the blocks 9 AM, and 10 AM will be grayed out. The block 11 AM will be colored red, and all blocks beyond 11 AM will be colored green. These time blocks will actively react to the time, and shift color without reload.
+
+Additionally, a user may enter text into a block-space, and click the button with the save-icon on the right-hand side of the screen. This will save the text into the user's local storage, and then print it back into the same text box that the user originally entered the text into. This will allow the user to exit and re-enter the page without losing their data. In turn, a user may find this application to aid in organization.
 
 ## Credits
 - [Changing elements based on time](https://stackoverflow.com/questions/40759946/change-element-based-on-time)
@@ -8,3 +21,9 @@ This app is meant to help schedule your workday from 9am through 5pm.
 - [Documentation of DayJS](https://day.js.org/docs/en/installation/installation)
 - [The addClass function in jquery](https://api.jquery.com/addclass/)
 - [The toggleClass attribute in jquery](https://api.jquery.com/toggleclass/)
+- [jQuery, a third-party API meant for simplifying JavaScript coding.](https://jquery.com/)
+- [Bootstrap, a third-party API meant for simplifying styling](https://getbootstrap.com/docs/5.1/getting-started/introduction/)
+- A personal tutoring session with Jude Clark, who co-authored sections of this code alongside me. Jude Clark was found through Washington University's FullStack Coding BootCamp tutoring program. 
+
+## License
+Please refer to the respective Github repository for licensing information.
